0e8748cd79fdd12a9f8c38543cbe57be8db02432
AnthonyLaurent1 <anthony061214@gmail.com>
2021-12-06 14:02:44 +0100

    create mailer episode and program

--- src/Controller/EpisodeController.php
+++ src/Controller/EpisodeController.php
@@ -10,6 +10,8 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -31,7 +33,7 @@ class EpisodeController extends AbstractController
      * @Route("/new", name="episode_new", methods={"GET", "POST"})
      * @param Slugify $slugify
      */
-    public function new(Request $request, EntityManagerInterface $entityManager, Slugify $slugify): Response
+    public function new(Request $request, EntityManagerInterface $entityManager, Slugify $slugify, MailerInterface $mailer): Response
     {
         $episode = new Episode();
         $form = $this->createForm(EpisodeType::class, $episode);
@@ -39,8 +41,18 @@ class EpisodeController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $episode->setSlug($slugify->generate($episode->getTitle()));
+            $episode->getSeason()->getProgram();
+
             $entityManager->persist($episode);
             $entityManager->flush();
+            $email = (new Email())
+                ->from($this->getParameter('mailer_from'))
+                ->to('your_email@example.com')
+                ->subject('Une nouvelle episode vient d\'être publiée !')
+                ->html($this->renderView('episode/newEpisodeEmail.html.twig',[
+                    'episode' => $episode
+                ]));
+            $mailer->send($email);
 
             return $this->redirectToRoute('episode_index', [], Response::HTTP_SEE_OTHER);
         }
